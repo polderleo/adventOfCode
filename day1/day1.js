e84c7584a22c8f90fa84bcafaea32929ec36358b
Leopold Stenger <leopold.stenger@uid.com>
2018-12-05 16:09:12 +0100

    optimize JS code

--- day1/day1.js
+++ day1/day1.js
@@ -1,12 +1,8 @@
 const fs = require('fs');
-
-var data = fs.readFileSync('input.txt');
-var dataString = data.toString();
-var dataArray = dataString.split('\r\n').map(Number);
-
+var data = fs.readFileSync('input.txt', 'utf-8');
+var dataArray = data.split('\r\n').map(Number);
 var result = 0;
-
-for(i = 0; i < dataArray.length; i++) {
+for(i = 0; i < dataArray.length ; i++) {
     result = result + dataArray[i];
-    console.log(result);
+    console.log(result)
 }
