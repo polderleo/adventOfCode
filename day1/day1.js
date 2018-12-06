f80dcecf034569b478535d0742450551db95bf08
Leopold Stenger <leopold.stenger@uid.com>
2018-12-06 17:58:18 +0100

    outsource to functions + part 2

--- day1/day1.js
+++ day1/day1.js
@@ -1,8 +1,40 @@
 const fs = require('fs');
-var data = fs.readFileSync('input.txt', 'utf-8');
-var dataArray = data.split('\r\n').map(Number);
-var result = 0;
-for(i = 0; i < dataArray.length ; i++) {
-    result = result + dataArray[i];
+
+function getNumbers() {
+    var data = fs.readFileSync('input.txt', 'utf-8');
+    var numbers = data.split('\r\n').map(Number);
+    console.log(numbers);
+    return numbers;
+}
+
+function part1() {
+    var numbers = getNumbers();
+    var result = 0;
+    for (i = 0; i < numbers.length; i++) {
+        result = result + numbers[i];
+    }
     console.log(result)
 }
+
+function part2() {
+    var numbers = getNumbers();
+    var result = 0;
+    var resultSet = new Set();
+    resultSet.add(0);
+    var i = 0;
+    while(true) {
+        result = result + numbers[i];
+        if(resultSet.has(result)) {
+            console.log('yeah rsult:' + result)
+            break;
+        }
+        resultSet.add(result);
+        i++;
+        if(i == numbers.length) {
+            i = 0;
+        }
+    }
+}
+
+part1();
+part2();
